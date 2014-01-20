101bb64bd2ff80588d9dab698719ce557a8ff4d7
Admin <admin@yourdomain.com>
2014-01-20 01:14:55 +0100

    ..

--- genkeys.sh
+++ genkeys.sh
@@ -4,12 +4,12 @@ cd $DIR
 echo "======== GENERATING SERVER CERTIFFICATE ========"
 echo $PWD
 if [ ! -d keys ]; then mkdir keys; fi
-if [ ! -e keys/server.key ]; then
-	openssl genrsa -out keys/server.key 1024
-	openssl rsa -in keys/server.key -out keys/server.key.out
-	mv keys/server.key.out keys/server.key
+if [ ! -e $PWD/keys/server.key ]; then
+	openssl genrsa -out $PWD/keys/server.key 1024
+	openssl rsa -in $PWD/keys/server.key -out $PWD/keys/server.key.out
+	mv $PWD/keys/server.key.out $PWD/keys/server.key
 
-	openssl req -subj "/C=US/ST=Oregon/L=Portland/O=IT/CN=`hostname`" -new -key keys/server.key -out keys/server.csr
+	openssl req -subj "/C=US/ST=Oregon/L=Portland/O=IT/CN=`hostname`" -new -key $PWD/keys/server.key -out $PWD/keys/server.csr
 
 	echo "################################################"
 	echo "# Please use keys/server.csr to sign the certifficate 
